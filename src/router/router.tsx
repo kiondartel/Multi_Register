fd3830aa47de2c2804b8c62a0d7666a27189c3c1
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-11-30 16:57:59 -0300

    initial

---
+++ src/router/router.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
