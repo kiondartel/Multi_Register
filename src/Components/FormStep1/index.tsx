a4028dad132f8e6456d9a848305611cb94f1c5c3
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-11-30 17:04:13 -0300

    Step1 & router

---
+++ src/Components/FormStep1/index.tsx
@@ -0,0 +1,7 @@
+import * as Styled from "./styles";
+
+const Step1 = () => {
+  return <Styled.Container></Styled.Container>;
+};
+
+export default Step1;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Step1 from "../Components/FormStep1";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Step1 />} />
       </Routes>
     </BrowserRouter>
   );
